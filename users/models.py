7958f067cbc3151ce629c6b9ddd4396d8420fc05
Vitor Galdino <httpvittor@gmail.com>
2023-04-10 17:13:51 -0300

    feat: add custom user model
    Create a custom user model that extends Django's AbstractUser. Add fields for email, first name, last name, birthdate, and is_employee. Override the save method to set the value of is_superuser based on the value of is_employee.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=127, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birthdate = models.DateField(null=True)
+    is_employee = models.BooleanField(default=False)
+
+    def save(self) -> None:
+        self.is_superuser = self.is_employee
+        return super().save()
