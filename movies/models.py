2ad6072fd5013db2bce5aedf53a0b1e2049e32d9
Vitor Galdino <httpvittor@gmail.com>
2023-04-11 19:37:50 -0300

    fix: adjust properties from attributes in model

--- movies/models.py
+++ movies/models.py
@@ -4,18 +4,18 @@ from django.db import models
 class RatingChoices(models.TextChoices):
     G_OPTION = 'G'
     PG_OPTION = 'PG'
-    PG_13_OPTION = ' PG-13'
+    PG_13_OPTION = 'PG-13'
     R_OPTION = 'R'
     NC_17_OPTION = 'NC-17'
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=127)
-    duration = models.CharField(max_length=10, null=True)
+    duration = models.CharField(max_length=10, null=True, default=None)
     rating = models.CharField(
-        max_length=20, choices=RatingChoices.choices, default=RatingChoices.G_OPTION
+        max_length=20, null=True, choices=RatingChoices.choices, default=RatingChoices.G_OPTION
     )
-    synopsis = models.TextField(null=True)
+    synopsis = models.TextField(null=True, default=None)
     user = models.ForeignKey(
         'users.User', on_delete=models.CASCADE, related_name='movies'
     )
