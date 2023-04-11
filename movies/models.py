dcaec1965eab7302d358d9ba08d4fb7f44df175c
Vitor Galdino <httpvittor@gmail.com>
2023-04-11 17:08:38 -0300

    feat: create Movie models

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Rating_Choices(models.TextChoices):
+    G_OPTION = 'G'
+    PG_OPTION = 'PG'
+    PG_13_OPTION = ' PG-13'
+    R_OPTION = 'R'
+    NC_17_OPTION = 'NC-17'
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=127)
+    duration = models.CharField(max_length=10, null=True)
+    rating = models.CharField(
+        max_length=20, choices=Rating_Choices.choices, default=Rating_Choices.G_OPTION
+    )
+    synopsis = models.TextField(null=True)
+    user = models.ForeignKey(
+        'users.User', on_delete=models.CASCADE, related_name='movies'
+    )
