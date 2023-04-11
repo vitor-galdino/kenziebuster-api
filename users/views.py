b61516bebe2a9e86f59fbbb064eab5a2e8bbf1d0
Vitor Galdino <httpvittor@gmail.com>
2023-04-10 23:54:49 -0300

    feat: create UserView and add POST route

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Response, status
 
-# Create your views here.
+from .models import User
+from .serializers import UserSerializer
+
+
+class UserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
