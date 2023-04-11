976e09fea2840506fff57f0b609a2b5dec04b851
Vitor Galdino <httpvittor@gmail.com>
2023-04-11 19:40:29 -0300

    feat: add post and get route in MovieView

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Response, status
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from .models import Movie
+from .permissions import IsSuperUserOrReadOnly
+
+from .serializers import MovieSerializer
+
+
+class MovieView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsSuperUserOrReadOnly]
+
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save(user=request.user)
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
+    
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+
+        return Response(serializer.data)
