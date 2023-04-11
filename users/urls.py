d46606ee4cb5b012c85db28fbd2480fabf8c0486
Vitor Galdino <httpvittor@gmail.com>
2023-04-11 16:22:30 -0300

    feat: create login route

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from . import views
 
 urlpatterns = [
     path('users/', views.UserView.as_view()),
+    path('users/login/', TokenObtainPairView.as_view()),
 ]
