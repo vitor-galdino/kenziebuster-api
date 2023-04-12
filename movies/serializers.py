5bea96f26c0084fb2d36e0a18c7287e44f47e231
Vitor Galdino <httpvittor@gmail.com>
2023-04-11 23:37:27 -0300

    feat: create MovieOrderSerializer from MovieOrderSerializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import RatingChoices, Movie
+from .models import MovieOrder, RatingChoices, Movie
 
 
 class MovieSerializer(serializers.Serializer):
@@ -15,3 +15,14 @@ class MovieSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
+
+
+class MovieOrderSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(source='movie.title', read_only=True)
+    price = serializers.DecimalField(max_digits=8, decimal_places=2)
+    buyed_by = serializers.EmailField(source='user.email', read_only=True)
+    buyed_at = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        return MovieOrder.objects.create(**validated_data)
