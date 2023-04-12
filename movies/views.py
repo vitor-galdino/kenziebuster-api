da6a060780cede0023218cbd55205f917527f2eb
Vitor Galdino <httpvittor@gmail.com>
2023-04-12 16:59:54 -0300

    refactor: include pagination in GET movie route

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView, Response, status
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -8,7 +9,7 @@ from .permissions import IsSuperUserOrReadOnly
 from .serializers import MovieOrderSerializer, MovieSerializer
 
 
-class MovieView(APIView):
+class MovieView(APIView, PageNumberPagination):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsSuperUserOrReadOnly]
 
@@ -21,10 +22,11 @@ class MovieView(APIView):
         return Response(serializer.data, status.HTTP_201_CREATED)
 
     def get(self, request):
-        movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True)
+        movies = Movie.objects.all().order_by('id')
+        result_page = self.paginate_queryset(movies, request)
+        serializer = MovieSerializer(result_page, many=True)
 
-        return Response(serializer.data)
+        return self.get_paginated_response(serializer.data)
 
 
 class MovieDetailView(APIView):
