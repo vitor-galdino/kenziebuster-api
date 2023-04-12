4c246e8bdfe0324ddf4c6f8f4f12d3a7acd4fb3b
Vitor Galdino <httpvittor@gmail.com>
2023-04-12 16:20:40 -0300

    feat: create new route from UserDetailView

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('users/', views.UserView.as_view()),
     path('users/login/', TokenObtainPairView.as_view()),
-]
+    path('users/<int:user_id>/', views.UserDetailView.as_view()),
+]   
