a2b5d0486edd8d266f1f2a2f8b3af21c580a4b35
Vitor Galdino <httpvittor@gmail.com>
2023-04-12 16:50:14 -0300

    feat: create UserDetailView and add GET and PATCH route

--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
 from rest_framework.views import APIView, Response, status
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 
 from .models import User
 from .serializers import UserSerializer
+from .permissions import IsAccountOwnerOrAdmin
+from django.shortcuts import get_object_or_404
 
 
 class UserView(APIView):
@@ -12,3 +16,24 @@ class UserView(APIView):
         serializer.save()
 
         return Response(serializer.data, status.HTTP_201_CREATED)
+
+class UserDetailView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwnerOrAdmin]
+
+    def get(self, request, user_id):
+        user = get_object_or_404(User, id=user_id)
+        self.check_object_permissions(request, user)
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
+    
+    def patch(self, request, user_id):
+        user = get_object_or_404(User, id=user_id)
+        self.check_object_permissions(request, user)
+        serializer = UserSerializer(user, request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data)
