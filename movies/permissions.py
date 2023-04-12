c68d90928f57b6fc06e8a8c384bdbe975e7357b2
Vitor Galdino <httpvittor@gmail.com>
2023-04-12 17:01:51 -0300

    refactor: permission sintax reduce

--- movies/permissions.py
+++ movies/permissions.py
@@ -1,8 +1,10 @@
 from rest_framework import permissions
 
+
 class IsSuperUserOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        
-        return request.user.is_authenticated and request.user.is_superuser
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+            and request.user.is_superuser
+        )
