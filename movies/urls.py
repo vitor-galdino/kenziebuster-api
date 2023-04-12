cc9ce2168980c2b34a0ad80a9fd3e2515b8d0d59
Vitor Galdino <httpvittor@gmail.com>
2023-04-11 23:56:10 -0300

    feat: add new route from MovieOrderView

--- movies/urls.py
+++ movies/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('movies/', views.MovieView.as_view()),
     path('movies/<int:movie_id>/', views.MovieDetailView.as_view()),
+    path('movies/<int:movie_id>/orders/', views.MovieOrderView.as_view()),
 ]
