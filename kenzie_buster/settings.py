cac61a7eb4385006a95f1918559418940bed83ca
Vitor Galdino <httpvittor@gmail.com>
2023-04-12 16:52:03 -0300

    feat: add pagination config

--- kenzie_buster/settings.py
+++ kenzie_buster/settings.py
@@ -140,3 +140,8 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 2,
+}
