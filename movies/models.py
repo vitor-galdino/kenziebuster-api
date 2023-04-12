2adad6cbc976f9684440cef80bced3473b268762
Vitor Galdino <httpvittor@gmail.com>
2023-04-11 23:24:57 -0300

    feat: create pivot table called MovieOrder

--- movies/models.py
+++ movies/models.py
@@ -13,9 +13,22 @@ class Movie(models.Model):
     title = models.CharField(max_length=127)
     duration = models.CharField(max_length=10, null=True, default=None)
     rating = models.CharField(
-        max_length=20, null=True, choices=RatingChoices.choices, default=RatingChoices.G_OPTION
+        max_length=20,
+        null=True,
+        choices=RatingChoices.choices,
+        default=RatingChoices.G_OPTION,
     )
     synopsis = models.TextField(null=True, default=None)
     user = models.ForeignKey(
         'users.User', on_delete=models.CASCADE, related_name='movies'
     )
+    users = models.ManyToManyField(
+        'users.User', through='MovieOrder', related_name='ordered_movies'
+    )
+
+
+class MovieOrder(models.Model):
+    movie = models.ForeignKey('movies.Movie', on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    buyed_at = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
